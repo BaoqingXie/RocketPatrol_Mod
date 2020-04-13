ae369fcfb2c8fcf5a2824ad3eeb1a1b3e5ae505b
BaoqingXie <bxie4@ucsc.edu>
2020-04-13 16:51:10 -0700

    last update

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -5,33 +5,39 @@ class Rocket extends Phaser.GameObjects.Sprite {
 
         scene.add.existing(this); //add to existing, diplaylist updatelist
         this.isFiring = false; //track rocket firing status
+        this.sfxRocket = scene.sound.add('sfx_rocket'); // add rocket sfx
     }
 
-    update(){
+    update() {
         //left and right movement
-        if(!this.isFiring){
-            if(keyLeft.isDown && this.x >= 47){
+        if (!this.isFiring) {
+            if (keyLeft.isDown && this.x >= 47) {
                 this.x -= 2;
-            }else if(keyRight.isDown && this.x<= 598){
+            } else if (keyRight.isDown && this.x <= 598) {
                 this.x += 2;
             }
         }
-        
-        //fire button(F)
-        if(Phaser.Input.Keyboard.JustDown(keyF)){
-            this.isFiring = true;
 
+        // fire button
+        if (Phaser.Input.Keyboard.JustDown(keyF) && !this.isFiring) {
+            this.isFiring = true;
+            this.sfxRocket.play();  // play sfx
         }
 
         //if fired, move up
-        if(this.isFiring && this.y >= 108){
+        if (this.isFiring && this.y >= 108) {
             this.y -= 2;
         }
 
         //reset on miss
-        if(this.y <= 108){
+        if (this.y <= 108) {
             this.isFiring = false;
             this.y = 431;
         }
+    }
+
+    reset() {
+        this.isFiring = false;
+        this.y = 431;
     }
 }
